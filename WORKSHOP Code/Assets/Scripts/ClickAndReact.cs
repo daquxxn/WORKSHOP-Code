20861c2f756bafceb7fc23de4ed6b0b2f6eedea1
daquxxn <50579443+daquxxn@users.noreply.github.com>
2020-11-02 16:05:44 +0100

    02-11 julie

---
+++ WORKSHOP Code/Assets/Scripts/ClickAndReact.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickAndReact : MonoBehaviour
+{
+    [SerializeField] private Slider _moodSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if(hit.transform != null)
+                {
+                    _moodSlider.value += 0.1f;
+                }
+            }
+        }
+    }
+}
