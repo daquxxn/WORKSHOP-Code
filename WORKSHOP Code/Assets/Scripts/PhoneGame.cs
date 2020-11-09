3cf677a16726f80ac74e552c076d5d06b2beefb1
daquxxn <50579443+daquxxn@users.noreply.github.com>
2020-11-09 22:43:08 +0100

    09/11

--- WORKSHOP Code/Assets/Scripts/PhoneGame.cs
+++ WORKSHOP Code/Assets/Scripts/PhoneGame.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhoneGame : MonoBehaviour
 {
-    [SerializeField] GameObject _phoneSprite = null;
-    private bool _isRinging = true;
+    public bool _isRinging = true;
 
     [SerializeField] private int _rotationSpeed = 10;
     [SerializeField] private int _dir = -1;
     [SerializeField] private float _clampRotation = 0f;
 
+    [SerializeField] private RectTransform _rectTrans = null;
+
     private AudioSource _audioSource = null;
+
+    private bool _tempRinging = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,19 +26,29 @@ public class PhoneGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
      if(_isRinging == true)
         {
-            if (_dir == 1 && transform.rotation.z >= _clampRotation / 1000)
+            if (_tempRinging == false)
+            {
+                _audioSource.Play();
+                _tempRinging = true;
+            }
+            if (_dir == 1 && _rectTrans.rotation.z >= _clampRotation / 1000)
             {
                 _dir = -1;
             }
-            else if (transform.rotation.z <= -_clampRotation / 1000)
+            else if (_rectTrans.rotation.z <= -_clampRotation / 1000)
             {
                 _dir = 1;
             }
-            transform.Rotate(Vector3.down * (_rotationSpeed * _dir * Time.deltaTime));
-
-            _audioSource.Play();
+            _rectTrans.Rotate(Vector3.forward * (_rotationSpeed * _dir * Time.deltaTime));
+            
+        }
+     else
+        {
+            _tempRinging = false;
+            _audioSource.Stop();
         }
     }
 }
