cafe40cff615c0655238565750e70f588a64fcdf
daquxxn <50579443+daquxxn@users.noreply.github.com>
2020-11-16 19:32:57 +0100

    16/11

---
+++ WORKSHOP Code/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    [SerializeField] private Transform _computerMarker = null;
+    [SerializeField] private Transform _phoneMarker = null;
+    [SerializeField] private Transform _coffeeMarker = null;
+    [SerializeField] private Transform _cameraBase = null;
+
+    [SerializeField]  private bool _phoneGameOn = false;
+    [SerializeField]  private bool _computerOn = false;
+    [SerializeField]  private bool _coffeeGameOn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_computerOn == true)
+        {
+            transform.position = Vector3.Lerp(transform.position, _computerMarker.position, Time.deltaTime);
+        }
+
+        if (_phoneGameOn == true)
+        {
+            transform.position = Vector3.Lerp(transform.position, _phoneMarker.position, Time.deltaTime);
+        }
+
+        if (_coffeeGameOn == true)
+        {
+            transform.position = Vector3.Lerp(transform.position, _coffeeMarker.position, Time.deltaTime);
+            transform.Rotate(Vector3.down, 20f * Time.deltaTime);
+        }
+
+        if(_coffeeGameOn == false && _phoneGameOn == false && _computerOn == false)
+        {
+            transform.position = Vector3.Lerp(transform.position, _cameraBase.position, Time.deltaTime);
+        }
+    }
+}
---
+++ WORKSHOP Code/Assets/Scripts/CoffeeGame.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeGame : MonoBehaviour
+{
+    [SerializeField] private Transform _farEnd = null;
+    private Vector3 _posA;
+    private Vector3 _posB;
+    [SerializeField] private float _speed = 1f;
+    private bool _podMoves = true;
+
+    [SerializeField] private float _podDist = 0f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _posA = transform.position;
+        _posB = _farEnd.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_podMoves == true)
+        {
+            transform.position = Vector3.Lerp(_posA, _posB, Mathf.PingPong(Time.time / _speed, 1f));
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+
+                if (hit.transform.tag == "CoffeePod")
+                {
+                    _podMoves = false;
+                    float step = _speed * Time.deltaTime;
+                    transform.position = transform.position + Vector3.down * _podDist;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("jenaimar");
+    }
+
+}
