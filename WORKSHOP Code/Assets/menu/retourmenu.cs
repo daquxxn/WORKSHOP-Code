ae2dd9034f5a340da9f2f7611bf6fa154c0f52b0
mathio74 <48549232+mathio74@users.noreply.github.com>
2021-01-03 22:50:51 +0100

    finish

---
+++ WORKSHOP Code/Assets/menu/retourmenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class retourmenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        // voir dans les options l'ordre des Scènes du jeu pour que ca marche
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
